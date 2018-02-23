044bc1c0b3340f0eebf9af5e5068bf72775c11bd
David Madaras <madaras.david1@gmail.com>
2018-02-23 19:27:08 +0100

    Log add

--- Sim1/SimGUI/Form1.cs
+++ Sim1/SimGUI/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace SimGUI
 {
@@ -15,6 +16,23 @@ namespace SimGUI
         public Form1()
         {
             InitializeComponent();
+            chart1.Series["Series2"].XValueType = ChartValueType.Int32;
+            chart1.Series["Series1"].XValueType = ChartValueType.Int32;
+            chart1.Series["Series1"].ChartType = SeriesChartType.Line;
+            chart1.Series["Series2"].ChartType = SeriesChartType.Line;
+
+            chart1.Series["Series2"].BorderWidth = 3;
+            //chart1.ChartAreas["0"].AxisX.Interval = 1;
+
+            chart1.Series["Series1"].BorderWidth = 3;
+            //chart1.ChartAreas["0"].AxisX.Interval = 1;
+
+        }
+        int last = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            chart1.Series["Series1"].Points.AddXY(last,new Random().Next(199));
+            chart1.Series["Series2"].Points.AddXY(last++, new Random().Next(199));
         }
     }
 }
---
+++ Sim1/SimLog/MainLog.cs
@@ -0,0 +1,26 @@
+﻿using SimCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimLog
+{
+    public class MainLog
+    {
+        private Core _core;
+
+        public MainLog() {
+
+            _core = new Core(10000, (x) => {
+                Thread.Sleep(500);
+                Console.WriteLine($"aaa {x}");
+            });
+            _core.OnEndEachReplication += new Core.EvntHandler(UpdateStatus);
+        }
+
+        
+    }
+}
